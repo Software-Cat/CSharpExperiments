4083fd08d3a73b9e99107a191bfa76a557a36d55
Software-Cat <51037053+Software-Cat@users.noreply.github.com>
2021-12-18 17:38:10 +1100

    Conform invalid user inputs

--- SimpleDataProcessing/Program.cs
+++ SimpleDataProcessing/Program.cs
@@ -4,14 +4,47 @@ internal class GlazerCalc
 {
     public static void Main()
     {
+        const double MAX_WIDTH = 5.0;
+        const double MAX_HEIGHT = 3.0;
+        const double MIN_WIDTH = .5;
+        const double MIN_HEIGHT = .75;
+
         double width, height, woodLength, glassArea;
         string widthString, heightString;
 
+        Console.WriteLine("Give the width of the window: ");
         widthString = Console.ReadLine() ?? string.Empty;
         width = double.Parse(widthString);
 
+        if (width < MIN_WIDTH)
+        {
+            Console.WriteLine("Width is too small.\n\n");
+            Console.WriteLine("Using minimum.");
+            width = MIN_WIDTH;
+        }
+        else if (width > MAX_WIDTH)
+        {
+            Console.WriteLine("Width is too large.\n\n");
+            Console.WriteLine("Using maximum.");
+            width = MAX_WIDTH;
+        }
+
+        Console.WriteLine("Give the height of the window: ");
         heightString = Console.ReadLine() ?? string.Empty;
         height = double.Parse(heightString);
+
+        if (height < MIN_HEIGHT)
+        {
+            Console.WriteLine("Height is too small.\n\n");
+            Console.WriteLine("Using minimum.");
+            height = MIN_HEIGHT;
+        }
+        else if (height > MAX_HEIGHT)
+        {
+            Console.WriteLine("Height is too large.\n\n");
+            Console.WriteLine("Using maximum.");
+            height = MAX_HEIGHT;
+        }
 
         woodLength = 2 * (width + height) * 3.25;
         glassArea = 2 * (width + height);
