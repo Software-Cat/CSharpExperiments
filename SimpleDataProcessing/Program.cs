b7f4e2c0bf6d0a37d5fc695900b6dcbd95609bb1
Software-Cat <51037053+Software-Cat@users.noreply.github.com>
2021-12-18 16:59:54 +1100

    Initial commit

--- SimpleDataProcessing/Program.cs
+++ SimpleDataProcessing/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+internal class GlazerCalc
+{
+    public static void Main()
+    {
+        double width, height, woodLength, glassArea;
+        string widthString, heightString;
+
+        widthString = Console.ReadLine() ?? string.Empty;
+        width = double.Parse(widthString);
+
+        heightString = Console.ReadLine() ?? string.Empty;
+        height = double.Parse(heightString);
+
+        woodLength = 2 * (width + height) * 3.25;
+        glassArea = 2 * (width + height);
+
+        Console.WriteLine("The length of the wood is " + woodLength + " feet.");
+        Console.WriteLine("The area of the glass is " + glassArea + " square meters.");
+    }
+}
